993adaff1eb9a991b6a9e7dfb6d9034e8c97c466
Lakshay <lakshayyadav2006@gmail.com>
2021-12-07 14:53:28 +0530

    Added comments to the file.

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def generate_password():
     max_length = int(input("\nEnter your desired password length: "))
     password_list = []
 
+    # add a random char from the COMBINED_LIST till the provided max password length has been reached
     for _ in range(max_length):
         password_list.append(random.choice(COMBINED_LIST))
 
+    # shuffle the list and print out the password to the terminal
     random.shuffle(password_list)
     password = "".join(password_list)
     print(f"Generated password: {password}")
